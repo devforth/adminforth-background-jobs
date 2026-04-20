a37d2db857cb19e7978fb40424612788e71c05aa
yaroslav8765 <pechorkin2014@gmail.com>
2026-04-20 16:44:21 +0300

    fix: make navbar icon color consistent

--- custom/NavbarJobs.vue
+++ custom/NavbarJobs.vue
@@ -3,7 +3,7 @@
     <div class="cursor-pointer hover:scale-110 transition-transform" @click="isDropdownOpen = !isDropdownOpen">
       <div class="relative flex items-center justify-center" v-if="jobs.length > 0">
         <Tooltip>
-          <IconBriefcaseSolid class="w-7 h-7 text-gray-600 hover:text-gray-700" />
+          <IconBriefcaseSolid class="w-7 h-7 text-lightNavbarIcons hover:text-darkNavbarIcons" />
           <template #tooltip>
             {{ t('All jobs completed') }}
           </template>
